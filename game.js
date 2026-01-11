b8530de1e672037235ab4ff8b87ff54d5a274aaa
yarascript-dot <yarascript@gmail.com>
2026-01-11 18:31:28 +0200

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
 // ===== НАЛАШТУВАННЯ =====
-const SCALE = 2.5;
+const SCALE = 1;
 const GROUND_Y = 150;
 
 // ===== ЗОБРАЖЕННЯ =====
